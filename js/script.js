aaaf4a201ee690d9943fedb079c57b4e605149ab
Eribaldo Oliveira <desenvolvedorwebst@gmail.com>
2023-04-05 16:52:00 -0300

    Funcionamento do JavaScript

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 
-const relogio = setInterval(function time(){
+const relogio = setInterval(function time() {
 	let datetoday = new Date(); 
 	let hr = datetoday.getHours();
 	let min = datetoday.getMinutes();
